6af12e2826430a45ddc7afbe68370266ac8e6868
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-18 15:44:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,7 +8,10 @@ class Shoe
   end
 
   def cobble
-    puts "Your shoe is as good as new!"
+    if @condition == "old"
+      @condtion = "new"
+      puts "Your shoe is as good as new!"
+    end
   end
 
 end
